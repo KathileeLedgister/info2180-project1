56790e2f626009e805d03fba02eba4d650b0deb9
KathileeLedgister <k.alex.ledgister@gmail.com>
2021-10-23 18:37:08 -0500

    Added script.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+
+function onclickSubmit(event){
+
+    let emailAdr= document.getElementById("email").value;
+    let message = "Please enter a valid email address.";
+
+    if(emailAdr){
+      message = "Thank you! Your email address &lt;"+ emailAdr + "&gt; has been added to our mailing list!";
+    }
+    document.querySelector("div.message").innerHTML = "<i>"+message+"</i>";
+    event.stopPropagation();
+    event.preventDefault();
+}
+
+window.onload = function() {
+    let elementBtn= document.querySelector(".newsletter .btn");
+
+    elementBtn.addEventListener("click", onclickSubmit);
+}
